02ed3453774ead029b54dbb7a81d5a164c748660
shibily <shibilysuneer10@gmail.com>
2025-02-19 01:23:41 +0530

    create update user api route

--- api/controllers/useController.js
+++ api/controllers/useController.js
@@ -1,5 +1,38 @@
+import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs'
+
 export const test = (req,res) => {
     res.json({
        message:'API working'  
     })
+}
+//update user
+
+export const updateUser = async(req,res,next) => {
+    if(req.user.id !==req.params.id){
+        // return res.status(401).json('you can update only your account')
+        return next(errorHandler(401,'you can update only your account'));
+    }
+    try {
+       if(req.body.password){
+        req.body.password =  bcryptjs.hash(req.body.password,10)
+       } 
+       const updatedUser = await User.findByIdAndUpdate(req.params.id,
+        {
+           $set:{
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password,
+            profilePicture:req.body.profilePicture,
+
+           } 
+        },
+        {new:true}
+       );
+       const {password,...rest} = updatedUser._doc
+       res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
 import authRoutes from './routes/authRoute.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 app.listen(3000, ()=>{
     console.log('server listening on port 3000');  
